b6b619113123508d3a41f3422497b87056418102
linziyan <aohan237@gmail.com>
2019-10-20 17:00:30 +0800

    [FEATURE]add aiohttp dependence

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 
-install_requires = ['python-snappy']
+install_requires = ['python-snappy', 'aiohttp']
 NAME = 'asyncnsq'
 PACKAGE = 'asyncnsq'
 PY_VER = sys.version_info
