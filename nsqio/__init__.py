52dc2a9e89df2da9d20628500dc50a431e709fcc
Yu Jing <yuikns@users.noreply.github.com>
2020-08-05 23:32:43 +0800

    [Update] bump version to 0.0.5

--- nsqio/__init__.py
+++ nsqio/__init__.py
@@ -1,6 +1,6 @@
 from nsqio.tcp.writer import create_writer
 from nsqio.tcp.reader import create_reader
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 __all__ = ["create_writer", "create_reader", "tcp", "http"]
